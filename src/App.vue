3673eb901623a5ff7640f9be0344144a32800e76
Ahmet Yavuz <yavuzahmet703@gmail.com>
2025-09-02 07:03:39 +0300

    ip store added2

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,12 @@
   import axios from 'axios'
   import { onMounted } from 'vue'
 
-  onMounted(() => {
-    axios.get('http://45.87.173.246:8080/log-visit')
+  onMounted(async () => {
+    try {
+      const res = await axios.get('http://45.87.173.246:8080/log-visit')
+      console.log('Ziyaret loglandı:', res.data)
+    } catch (error) {
+      console.error('Hata oluştu:', error)
+    }
   })
 </script>
